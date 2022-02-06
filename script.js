c5f679227c416bb9d1878dbb857fc71541b31376
Samaheh Loghmani <>
2022-02-05 21:17:44 -0500

    add function for delete and complete todo item

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const trashButton = document.querySelector(".trash-btn");
 
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCompletedTodo);
+
 // functinality for adding todo list
 function addTodo(event) {
     event.preventDefault();
@@ -12,6 +15,8 @@ function addTodo(event) {
 
     const newTodo = document.createElement("li");
     newTodo.innerText = todoInput.value;
+
+    saveLocalTodo(todoInput.value);
 
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
@@ -28,4 +33,29 @@ function addTodo(event) {
     todoDiv.appendChild(trashButton);
 
     todoList.appendChild(todoDiv);
+}
+
+function deleteCompletedTodo(event) {
+    console.log(event.target.classList[0]);
+    const item = event.target;
+    if (item.classList[0] === "trash-btn") {
+        item.parentElement.remove();
+        console.log(item.parentElement);
+    }
+    if (item.classList[0] === "complete-btn") {
+        item.parentElement.classList.toggle("completed");
+    }
+}
+
+// function for saving local storage
+
+function saveLocalTodo(todo) {
+    let todos; // value in localstorage
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
