925964eddcc207a7a1539d147b3c2122dbe04e76
Samaheh Loghmani <>
2022-02-05 19:24:41 -0500

    add comment

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
 
 todoButton.addEventListener("click", addTodo);
-
+// functinality for adding todo list
 function addTodo(event) {
     event.preventDefault();
 
